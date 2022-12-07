156e9b650d0cc85e7ea6ee24733099d013f8b519
Prathwik <64214685+prathwik0@users.noreply.github.com>
2022-12-07 15:00:45 +0530

    Create linkedlist.c

---
+++ linkedlist.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct NODE
+{
+    int data;
+    struct NODE *next;
+};
+
+typedef struct NODE *node;
+
+void getnode(node *n)
+{
+    *n = (struct NODE *)malloc(sizeof(struct NODE));
+}
+
+void insert(node *n, int x)
+{
+    node temp = *n;
+    getnode(n);
+    (*n)->data = x;
+    (*n)->next = temp;
+}
+
+void insertend(node *n, int x)
+{
+    while(*n != NULL)
+        n = &((*n)->next);
+    getnode(n);
+    (*n)->data = x;
+    (*n)->next = NULL;
+}
+
+void display(node *n)
+{
+    printf("\n");
+    while(*n != NULL)
+    {
+        printf("%d ", (*n)->data);
+        n = &((*n)->next);
+    }
+    printf("\n");
+    return;
+}
+
+int delete(node *n, int x)
+{
+    while(*n != NULL)
+    {
+        if ((*n)->data == x)
+        {
+            node temp = *n;
+            *n = (*n)->next;
+            free(temp);
+        }
+        n = &((*n)->next);
+    }
+    return 0;
+}
+
+int main()
+{
+    node a = NULL;
+
+    while (1)
+    {
+        int n;
+        printf("1 - insert, 2 - display, 3 - delete\n");
+        scanf("%d", &n);
+
+        if (n == 1)
+        {
+            int d;
+            printf("Enter int: ");
+            scanf("%d", &d);
+            insertend(&a, d);
+        }
+        else if (n == 2)
+        {
+            display(&a);
+        }
+        else if (n == 3)
+        {
+            int d;
+            printf("Enter int: ");
+            scanf("%d", &d);
+            delete(&a, d);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
