8707fc0f21cff138e8b99ea79ac9658d02bfd04d
Prathwik <64214685+prathwik0@users.noreply.github.com>
2022-12-24 12:52:14 +0530

    Update tree_traversal.c to include Level Order traversal

--- dsclass/tree_traversal.c
+++ dsclass/tree_traversal.c
@@ -43,6 +43,44 @@ void inorder(node root){
     printf("%d ", root->data);
     inorder(root->right);
 }
+
+//-------------------This is the code for the queue------------------------//
+typedef struct NODEQ
+{
+    node d;
+    struct NODEQ *next;
+}*nodeq;
+
+void getnode(nodeq *n)
+{
+    *n = (struct NODEQ *)malloc(sizeof(struct NODEQ));
+}
+
+typedef struct 
+{
+    nodeq front; 
+    nodeq rear;  
+}queue;
+
+void enqueue(queue *q, node x);
+node dequeue(queue *q);
+
+//-------------------This is the code for the queue------------------------//
+
+void levelorder(node root){
+    queue q;
+    q.front = NULL;
+    q.rear = NULL;
+
+    while(root != NULL)
+    {
+        printf("%d ", root->data);
+        enqueue(&q, root->left);
+        enqueue(&q, root->right);
+
+        root = dequeue(&q);
+    }
+}
  
 int main()
 {
@@ -69,6 +107,60 @@ int main()
 
     printf("\nInorder:   ");
     inorder(root);
+
+    printf("\nLevelorder:   ");
+    levelorder(root);
     
     return 0;
+}
+
+void enqueue(queue *q, node x)
+{
+    //this initializes the queue if there are no elements
+    if (q->front == NULL)
+    {
+        getnode(&(q->front));
+        q->front->next = q->front;
+        q->rear = q->front->next;
+
+        q->front->d = x;
+    }
+    else
+    {
+        getnode(&(q->front->next));
+
+        q->front->next->d = x;
+        q->front->next->next = q->rear;
+
+        //this sets front to the new front 
+        q->front = q->front->next;
+
+        //we could have done the above before initializing the new nodeq but 
+        //but then I wouldn't get to use ...->...->...->...
+    }
+}
+
+node dequeue(queue *q)
+{
+    if (q->front == NULL)
+    {
+        return NULL;
+    }
+
+    if (q->rear == q->front)
+    {
+        node temp = q->front->d;
+        free(q->front);
+        q->front = NULL;
+        q->rear = NULL;
+        return temp;
+    }
+
+    q->rear = q->rear->next;
+
+    node temp = q->front->next->d;
+    free(q->front->next);
+    q->front->next = q->rear;
+
+    return temp;
 }
