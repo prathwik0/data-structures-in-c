7d03d8f3c4065bd927f6b84dfcba2f614f8f62a2
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-08 13:50:19 +0530

    Create user_input.c

---
+++ linked_list/user_input.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "singly.h"
+
+int main()
+{
+    list a;
+    a.front = NULL;
+
+    while (1)
+    {
+        int choice;
+        printf("1 - insert, 2 - display, 3 - delete : ");
+        scanf("%d", &choice);
+
+        if (choice == 1)
+        {
+            int data;
+            scanf("%d", &data);
+
+            insertRear(&a, data);
+        }
+        else if (choice == 2)
+        {
+            display(&a);
+        }
+        else if (choice == 3)
+        {
+            int key;
+            scanf("%d", &key);
+            deletePosition(&a, key);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
