134516850f68667f71d1d2d9e3db5cb11123d148
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-09 18:08:35 +0530

    Create priority.c

---
+++ abhishek/priority.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+
+int queue[N];
+int rear = -1;
+int front = -1;
+
+void insert(int x)
+{
+    if (rear == N - 1)
+    {
+        printf("full");
+    }
+    else if (front == -1 && rear == -1)
+    {
+        front = 0;
+        rear = 0;
+        queue[rear] = x;
+    }
+    else
+    {
+        rear++;
+        printf("rear = %d\n", rear);
+        queue[rear] = x;
+    }
+}
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort()
+{
+    for (int i = front; i < rear; i++)
+    {
+        for (int j = i + 1; j <= rear; j++)
+        {
+            if (queue[j] < queue[i])
+            {
+                swap(&queue[j], &queue[i]);
+            }
+        }
+    }
+}
+
+// void sort()
+// {
+//     int i, j;
+//     for (i = front; i < rear; i++)
+//         for (j = 0; j < rear - 1; j++)
+//             if (queue[j] > queue[j + 1])
+//                 swap(&queue[j], &queue[j + 1]);
+// }
+
+void dequeue()
+{
+    if (front == -1 && rear == -1)
+    {
+        printf("empty");
+    }
+    else if (front == rear)
+    {
+        printf("%d\n", queue[front]);
+        front = rear = -1;
+    }
+    else
+    {
+        printf("%d\n", queue[front]);
+        front++;
+    }
+}
+
+void display()
+{
+    int i;
+
+    if (front == -1 && rear == -1)
+    {
+        printf("empty");
+    }
+    else
+    {
+        for (i = front; i < rear + 1; i++)
+        {
+            printf("%d\n", queue[i]);
+        }
+    }
+}
+
+void main()
+{
+    int ch, x;
+
+    while (1)
+    {
+        printf("1.INSERT \n 2.DEQUEUE \n 3. DISPLAY\n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+
+        case 1:
+            printf("enter the elemebnt: ");
+            scanf("%d", &x);
+            insert(x);
+            sort();
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        default:
+            printf("wrong operation");
+        }
+    }
+}
