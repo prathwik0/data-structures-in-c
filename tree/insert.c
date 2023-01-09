589bda12b2d142601f544cc0f251633d99b6bce4
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-09 14:26:26 +0530

    create unbalanced BST insert module

---
+++ tree/insert.c
@@ -0,0 +1,25 @@
+#include "tree_insert.h"
+#include "treeNode.h"
+
+void insert(node *root, int data)
+{
+    if (*root == NULL)
+    {
+        *root = getNode(data);
+        return;
+    }
+
+    if ((*root)->data == data)
+    {
+        return;
+    }
+
+    if ((*root)->data > data)
+    {
+        insert(&((*root)->left), data);
+    }
+    else
+    {
+        insert(&((*root)->right), data);
+    }
+}
---
+++ tree/insert.h
@@ -0,0 +1,11 @@
+#ifndef INSERT_H
+#define INSERT_H
+
+#include "treeNode.h"
+
+/*
+ *unbalanced BST insertion
+ */
+void insert(node *root, int data);
+
+#endif
