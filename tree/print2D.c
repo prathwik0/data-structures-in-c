487a38e560d64be1ed4a1c1364bf3932653a33e8
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-09 14:06:17 +0530

    move files

---
+++ tree/print2D.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "print2D.h"
+
+typedef struct NODE
+{
+    struct NODE *left;
+    int data;
+    struct NODE *right;
+} *node;
+
+void print2DUtil(node root, int space)
+{
+    // Base case
+    if (root == NULL)
+        return;
+
+    // Increase distance between levels
+    space += COUNT;
+
+    // Process right child first
+    print2DUtil(root->right, space);
+
+    // Print current node after space
+    // count
+    printf("\n");
+    for (int i = COUNT; i < space; i++)
+        printf(" ");
+    printf("%d", root->data);
+
+    // Process left child
+    print2DUtil(root->left, space);
+}
+
+// Wrapper over print2DUtil()
+void print2D(struct NODE *root)
+{
+    // Pass initial space count as 0
+    print2DUtil(root, 0);
+}
---
+++ tree/tree_insert.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree_insert.h"
+
+node return_node(int data)
+{
+    node x = (struct NODE *)malloc(sizeof(struct NODE));
+    x->left = NULL;
+    x->data = data;
+    x->right = NULL;
+    return x;
+}
+
+void insert_node(node *x, int data)
+{
+    *x = (struct NODE *)malloc(sizeof(struct NODE));
+    (*x)->left = NULL;
+    (*x)->data = data;
+    (*x)->right = NULL;
+}
+
+void inorder(node root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int get_height(node root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int x = get_height(root->left);
+    int y = get_height(root->right);
+
+    return x > y ? x + 1 : y + 1;
+}
+
+void insert_without_arranging(node *root, int data)
+{
+    if (*root == NULL)
+    {
+        *root = return_node(data);
+        return;
+    }
+
+    if ((*root)->data > data)
+    {
+        insert_without_arranging(&((*root)->left), data);
+    }
+    else
+    {
+        insert_without_arranging(&((*root)->right), data);
+    }
+}
+
+void arrange(node *root);
+void clockwise_rotate(node *root);
+void anticlockwise_rotate(node *root);
+
+void insert_with_arranging(node *root, int data)
+{
+    if (*root == NULL)
+    {
+        *root = return_node(data);
+        return;
+    }
+
+    if ((*root)->data > data)
+    {
+        insert_with_arranging(&((*root)->left), data);
+    }
+    else
+    {
+        insert_with_arranging(&((*root)->right), data);
+    }
+
+    arrange(root);
+}
+
+void arrange(node *root)
+{
+    int l = get_height((*root)->left);
+    int r = get_height((*root)->right);
+
+    if (l - r == 0 || l - r == 1 || l - r == -1)
+    {
+        return;
+    }
+
+    if (r > l)
+    {
+        if (get_height((*root)->right->right) > get_height((*root)->right->left))
+        {
+            anticlockwise_rotate(root);
+        }
+        else
+        {
+            clockwise_rotate(&((*root)->right));
+            anticlockwise_rotate(root);
+        }
+    }
+
+    if (l > r)
+    {
+        if (get_height((*root)->left->left) > get_height((*root)->left->right))
+        {
+            clockwise_rotate(root);
+        }
+        else
+        {
+            anticlockwise_rotate(&((*root)->left));
+            clockwise_rotate(root);
+        }
+    }
+}
+
+void clockwise_rotate(node *root)
+{
+    node temp = (*root)->left->right;
+
+    (*root)->left->right = *root;
+    *root = (*root)->left;
+
+    (*root)->right->left = temp;
+}
+
+void anticlockwise_rotate(node *root)
+{
+    node temp = (*root)->right->left;
+
+    (*root)->right->left = *root;
+    *root = (*root)->right;
+
+    (*root)->left->right = temp;
+}
