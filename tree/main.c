f04788a1fca209dd387a2491125ec90c56ef7b90
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-09 11:35:47 +0530

    tree insertion module creation

---
+++ tree/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "print2D.h"
+#include "tree_insert.h"
+
+int main()
+{
+    node root = NULL;
+
+    insert_with_arranging(&(root), 1);
+    insert_with_arranging(&(root), 2);
+    insert_with_arranging(&(root), 3);
+    insert_with_arranging(&(root), 4);
+    insert_with_arranging(&(root), 5);
+    insert_with_arranging(&(root), 6);
+    insert_with_arranging(&(root), 7);
+    insert_with_arranging(&(root), 8);
+    insert_with_arranging(&(root), 9);
+    insert_with_arranging(&(root), 10);
+    insert_with_arranging(&(root), 9);
+    insert_with_arranging(&(root), 7);
+    // insert_with_arranging(&(root), 13);
+    // insert_with_arranging(&(root), 14);
+    // insert_with_arranging(&(root), 15);
+    // insert_with_arranging(&(root), 16);
+    // insert_with_arranging(&(root), 17);
+
+    printf("\nInorder:   ");
+    inorder(root);
+
+    printf("\nHeight:    %d", get_height(root));
+
+    print2D(root);
+
+    return 0;
+}
--- tree/tree_insert.c
+++ tree/tree_insert.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define COUNT 4
-
-typedef struct NODE
-{
-    struct NODE *left;
-    int data;
-    struct NODE *right;
-} *node;
+#include "print2D.h"
 
 node return_node(int data)
 {
@@ -26,53 +19,6 @@ void insert_node(node *x, int data)
     (*x)->right = NULL;
 }
 
-typedef struct
-{
-    node root;
-} tree;
-
-void inorder(node root);
-void insert_without_arranging(node *root, int data);
-void insert_with_arranging(node *root, int data);
-void arrange(node *root);
-int get_height(node root);
-void clockwise_rotate(node *root);
-void anticlockwise_rotate(node *root);
-void print2DUtil(node root, int space);
-void print2D(node root);
-
-int main()
-{
-    node root = NULL;
-
-    insert_with_arranging(&(root), 1);
-    insert_with_arranging(&(root), 2);
-    insert_with_arranging(&(root), 3);
-    insert_with_arranging(&(root), 4);
-    insert_with_arranging(&(root), 5);
-    insert_with_arranging(&(root), 6);
-    insert_with_arranging(&(root), 7);
-    insert_with_arranging(&(root), 8);
-    insert_with_arranging(&(root), 9);
-    insert_with_arranging(&(root), 10);
-    insert_with_arranging(&(root), 9);
-    insert_with_arranging(&(root), 7);
-    // insert_with_arranging(&(root), 13);
-    // insert_with_arranging(&(root), 14);
-    // insert_with_arranging(&(root), 15);
-    // insert_with_arranging(&(root), 16);
-    // insert_with_arranging(&(root), 17);
-
-    printf("\nInorder:   ");
-    inorder(root);
-
-    printf("\nHeight:    %d", get_height(root));
-
-    print2D(root);
-
-    return 0;
-}
-
 void inorder(node root)
 {
     if (root == NULL)
@@ -82,6 +28,19 @@ void inorder(node root)
     inorder(root->left);
     printf("%d ", root->data);
     inorder(root->right);
+}
+
+int get_height(node root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int x = get_height(root->left);
+    int y = get_height(root->right);
+
+    return x > y ? x + 1 : y + 1;
 }
 
 void insert_without_arranging(node *root, int data)
@@ -101,6 +60,10 @@ void insert_without_arranging(node *root, int data)
         insert_without_arranging(&((*root)->right), data);
     }
 }
+
+void arrange(node *root);
+void clockwise_rotate(node *root);
+void anticlockwise_rotate(node *root);
 
 void insert_with_arranging(node *root, int data)
 {
@@ -177,47 +140,4 @@ void anticlockwise_rotate(node *root)
     *root = (*root)->right;
 
     (*root)->left->right = temp;
-}
-
-int get_height(node root)
-{
-    if (root == NULL)
-    {
-        return 0;
-    }
-
-    int x = get_height(root->left);
-    int y = get_height(root->right);
-
-    return x > y ? x + 1 : y + 1;
-}
-
-void print2DUtil(node root, int space)
-{
-    // Base case
-    if (root == NULL)
-        return;
-
-    // Increase distance between levels
-    space += COUNT;
-
-    // Process right child first
-    print2DUtil(root->right, space);
-
-    // Print current node after space
-    // count
-    printf("\n");
-    for (int i = COUNT; i < space; i++)
-        printf(" ");
-    printf("%d", root->data);
-
-    // Process left child
-    print2DUtil(root->left, space);
-}
-
-// Wrapper over print2DUtil()
-void print2D(node root)
-{
-    // Pass initial space count as 0
-    print2DUtil(root, 0);
 }
