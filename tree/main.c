1a16932c60854c2eb8edb0454342fb9443b06737
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-09 15:38:39 +0530

    Update main.c

--- tree/main.c
+++ tree/main.c
@@ -5,30 +5,35 @@
 #include "traversal.h"
 #include "print2D.h"
 #include "insert.h"
+#include "search.h"
 
 int main()
 {
-    /*gives the seed for the rng*/
-    // time_t t;
-    // srand((unsigned)time(&t));
+    // this displays the time taken for the function to execute
+    // start the clock
+    clock_t start, end;
+    double elapsed;
+    start = clock();
 
+    /*gives the seed for the rng*/
+    time_t t;
+    srand((unsigned)time(&t));
+
+    // initializes the root node
     node root = NULL;
 
-    // inserts 10 integers
-    for (int i = 0; i < 10; i++)
+    // gets 25 integers
+    int num[25];
+    for (int i = 0; i < 25; i++)
     {
-        int num = rand() % 100;
-        printf("inserted %d\n", num);
-        insert(&root, num);
+        num[i] = rand() % 1000;
     }
 
-    //                 (3)
-    //            /            \
-    //         (2)              (5)
-    //       /      \          /    \
-    //    (1)        (4)     (-)     (-)
-    //   /   \      /   \
-    // (10) (11)  (12)  (13)
+    for (int i = 0; i < 10; i++)
+    {
+        printf("inserted %d\n", num[i]);
+        insert(&root, num[i]);
+    }
 
     printf("\nPreorder:   ");
     preorder(root);
@@ -44,6 +49,27 @@ int main()
 
     printf("\n2D: ");
     print2D(root);
+
+    printf("\n\n");
+
+    // search for a node and print the result
+    node temp;
+    temp = rsearch(root, 100);
+    if (temp == NULL)
+        printf("100 was not found\n");
+    else
+        printf("Node found - %d\n", temp->data);
+
+    temp = rsearch(root, num[3]);
+    if (temp == NULL)
+        printf("%d was not found\n", num[3]);
+    else
+        printf("Node found - %d\n", temp->data);
+
+    // stop the clock and calculate elapsed time
+    end = clock();
+    elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Elapsed time: %f seconds\n", elapsed);
 
     return 0;
 }
