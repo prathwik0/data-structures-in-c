8978737ef6eb66c76bb125780d9008d7c5511242
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-10 12:10:27 +0530

    Update data.c

--- avl_tree/data.c
+++ avl_tree/data.c
@@ -5,3 +5,19 @@ void printdata(data x)
 {
     printf("%d", x->i);
 }
+
+int datacmp(data x, data y)
+{
+    if (x->i == y->i)
+    {
+        return 0;
+    }
+    else if (x->i > y->i)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
