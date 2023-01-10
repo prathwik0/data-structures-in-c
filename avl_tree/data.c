8ff14000051ec22fdb53df4f4e66c612fca7b1b7
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-10 12:40:10 +0530

    Update data.c

--- avl_tree/data.c
+++ avl_tree/data.c
@@ -21,3 +21,19 @@ int datacmp(data x, data y)
         return -1;
     }
 }
+
+data getdata(int n)
+{
+    /*gives the seed for the rng*/
+    time_t t;
+    srand((unsigned)time(&t));
+
+    data x = (struct DATA *)malloc(sizeof(struct DATA)*n);
+
+    for (int i = 0; i < n; i++)
+    {
+        x[i].i = rand() % 1000;
+    }
+
+    return x;
+}
