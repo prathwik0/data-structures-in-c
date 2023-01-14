77641aec505932aeefd6eb9192da9a01abce4809
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-14 20:12:53 +0530

    Create tempCodeRunnerFile.c

---
+++ kaustub/tempCodeRunnerFile.c
@@ -0,0 +1,23 @@
+// if opening brackt push to stak
+        // else if (*ch == '(')
+        // {
+        //     push(&s, *ch);
+        // }
+        // // if stack empty push to stack
+        // else if (peek(&s) == '0')
+        // {
+        //     push(&s, *ch);
+        // }
+        // else
+        // {
+        //     operator= peek(&s);
+
+        //     if (precedence(operator) < precedence(*ch))
+        //     {
+        //         push(&s, *ch);
+        //     }
+        //     else if (precedence(operator) == precedence(*ch))
+        //     {
+
+        //     }
+        // }
