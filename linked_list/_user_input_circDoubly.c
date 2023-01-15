8ea852fab803e4674b7e5629b125815898c09031
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-15 15:40:49 +0530

    Create _user_input_circDoubly.c

---
+++ linked_list/_user_input_circDoubly.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "circDoubly.h"
+
+int main()
+{
+    list a;
+    a.front = NULL;
+
+    int ch, x;
+
+    while (1)
+    {
+        printf("1 - insert, 2 - display, 3 - delete : ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            scanf("%d", &x);
+            insertFront(&a, x);
+            break;
+        case 2:
+            display(&a);
+            break;
+        case 3:
+            deleteRear(&a);
+            break;
+        default:
+            return 0;
+        }
+    }
+
+    return 0;
+}
